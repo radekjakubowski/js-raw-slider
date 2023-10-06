c36023525191d98c5b1527537dbd58577e170024
Radek Jakubowski <jakubowskiumieszcz@gmail.com>
2023-10-06 10:05:30 +0200

    added play/pause toggle

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const innerScrollerItemsChildren = document.querySelectorAll("#scroller-inner__items > *");
 const innerScrollerItemsParent = document.querySelector("#scroller-inner__items");
+const playPauseControl = document.querySelector("#scroller-inner__controls__play-pause");
 
 innerScrollerItemsChildren.forEach(child => {
     const clonedChild = child.cloneNode(true);
     innerScrollerItemsParent.appendChild(clonedChild);
 });
+
+playPauseControl.addEventListener('click', toggleAnimationState)
+
+function toggleAnimationState() {
+    let animationState = innerScrollerItemsParent.style['animationPlayState'];
+
+    if (animationState === 'paused') {
+        innerScrollerItemsParent.style['animationPlayState'] = 'running';
+        return;
+    }
+
+    innerScrollerItemsParent.style['animationPlayState'] = 'paused';
+}
